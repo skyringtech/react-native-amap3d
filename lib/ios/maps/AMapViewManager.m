fe9401b68cb553056643ec19c7d8c777c602c419
Ning Chen <c.ning0@gmail.com>
2020-04-29 22:56:21 +0800

    Move changes found in the 2.0.1-0 npm release only into repo.

--- lib/ios/maps/AMapViewManager.m
+++ lib/ios/maps/AMapViewManager.m
@@ -157,13 +157,11 @@ RCT_EXPORT_METHOD(setStatus:(nonnull NSNumber *)reactTag params:(NSDictionary *)
     if (mapView.onStatusChange) {
         MAMapStatus *status = mapView.getMapStatus;
         mapView.onStatusChange(@{
-                @"center": @{
-                    @"latitude": @(status.centerCoordinate.latitude),
-                    @"longitude": @(status.centerCoordinate.longitude),
-                },
                 @"zoomLevel": @(status.zoomLevel),
                 @"tilt": @(status.cameraDegree),
                 @"rotation": @(status.rotationDegree),
+                @"latitude": @(status.centerCoordinate.latitude),
+                @"longitude": @(status.centerCoordinate.longitude),
         });
     }
 }
@@ -172,19 +170,13 @@ RCT_EXPORT_METHOD(setStatus:(nonnull NSNumber *)reactTag params:(NSDictionary *)
     if (mapView.onStatusChangeComplete) {
         MAMapStatus *status = mapView.getMapStatus;
         mapView.onStatusChangeComplete(@{
-                @"center": @{
-                    @"latitude": @(status.centerCoordinate.latitude),
-                    @"longitude": @(status.centerCoordinate.longitude),
-                },
-                @"region": @{
-                    @"latitude": @(status.centerCoordinate.latitude),
-                    @"longitude": @(status.centerCoordinate.longitude),
-                    @"latitudeDelta": @(mapView.region.span.latitudeDelta),
-                    @"longitudeDelta": @(mapView.region.span.longitudeDelta),
-                },
                 @"zoomLevel": @(status.zoomLevel),
                 @"tilt": @(status.cameraDegree),
                 @"rotation": @(status.rotationDegree),
+                @"latitude": @(status.centerCoordinate.latitude),
+                @"longitude": @(status.centerCoordinate.longitude),
+                @"latitudeDelta": @(mapView.region.span.latitudeDelta),
+                @"longitudeDelta": @(mapView.region.span.longitudeDelta),
         });
     }
 }
